33d0f3228ea8d5b23f85b7ea6ae0bef2388c5333
XyuiopGameGuy <92894184+XyuiopGameGuy@users.noreply.github.com>
2021-10-20 19:49:31 -0300

    Update sketch.js

--- Trex_Stage_0-main/sketch.js
+++ Trex_Stage_0-main/sketch.js
@@ -1,19 +1,30 @@
+ var trex;
+ var trexAnimation;
+ var ground;
+ var chao;
 
-var trex ,trex_running;
-function preload(){
-  
-
+ function preload(){
+  trexAnimation= loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  ground= loadImage("ground2.png");
 }
 
 function setup(){
-  createCanvas(600,200)
-  
-  //create a trex sprite
- 
+  createCanvas(600,200);
+  trex= createSprite(100, 101, 89, 94) ;
+  trex.addAnimation("running", trexAnimation);
+  trex.scale= 0.7
+
+  chao = createSprite(300, 100, 600, 26);
+  chao.addImage(ground);
 }
 
 function draw(){
-  background("white")
-  
+  background("white");
+  chao.velocityX= -5
+  trex.collide(chao)
+  if(keyDown("space")){
+    trex.velocityY= -3
+  }
 
+  drawSprites();
 }
