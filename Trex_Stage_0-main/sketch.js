88ea0b1824190dce537d814bd29af0f32deebcd9
XyuiopGameGuy <92894184+XyuiopGameGuy@users.noreply.github.com>
2021-10-20 20:14:07 -0300

    Update sketch.js

--- Trex_Stage_0-main/sketch.js
+++ Trex_Stage_0-main/sketch.js
@@ -1,4 +1,4 @@
- var trex;
+  var trex;
  var trexAnimation;
  var ground;
  var chao;
@@ -10,21 +10,25 @@
 
 function setup(){
   createCanvas(600,200);
-  trex= createSprite(100, 101, 89, 94) ;
+  trex= createSprite(100, 200, 89, 94) ;
   trex.addAnimation("running", trexAnimation);
   trex.scale= 0.7
 
-  chao = createSprite(300, 100, 600, 26);
+  chao = createSprite(300, 200, 600, 26);
   chao.addImage(ground);
 }
 
 function draw(){
   background("white");
   chao.velocityX= -5
-  trex.collide(chao)
   if(keyDown("space")){
-    trex.velocityY= -3
+    trex.velocityY=-0.8
+    trex.velocityY=0.8
   }
-
+  trex.velocityY = trex.velocityY = +0.8
+  if (chao.x < 0){
+    chao.x= chao.width/2;
+  } 
+  trex.collide(chao)
   drawSprites();
 }
