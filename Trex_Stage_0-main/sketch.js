d2a7847b2a1a3e41bd5e61f567cfbffac269dd90
XyuiopGameGuy <92894184+XyuiopGameGuy@users.noreply.github.com>
2021-10-25 19:50:16 -0300

    Update sketch.js

--- Trex_Stage_0-main/sketch.js
+++ Trex_Stage_0-main/sketch.js
@@ -1,7 +1,9 @@
-  var trex;
+  
+ var trex;
  var trexAnimation;
  var ground;
  var chao;
+ var chaoinvisivel;
 
  function preload(){
   trexAnimation= loadAnimation("trex1.png", "trex3.png", "trex4.png");
@@ -10,25 +12,29 @@
 
 function setup(){
   createCanvas(600,200);
-  trex= createSprite(100, 200, 89, 94) ;
+  trex= createSprite(100, 190, 89, 94) ;
   trex.addAnimation("running", trexAnimation);
   trex.scale= 0.7
 
-  chao = createSprite(300, 200, 600, 26);
-  chao.addImage(ground);
+  chao = createSprite(300, 180, 600, 26);
+  chao.addImage(ground); 
+  chaoinvisivel= createSprite(300, 190, 600, 10);
+  chaoinvisivel.visible=false;
 }
 
 function draw(){
   background("white");
   chao.velocityX= -5
-  if(keyDown("space")){
-    trex.velocityY=-0.8
-    trex.velocityY=0.8
+  if(keyDown("space") && trex.y > 152){
+    trex.velocityY=-13
   }
-  trex.velocityY = trex.velocityY = +0.8
+  trex.velocityY=trex.velocityY +0.8
   if (chao.x < 0){
     chao.x= chao.width/2;
   } 
-  trex.collide(chao)
+  trex.collide(chaoinvisivel);
+  console.log(trex.y);
   drawSprites();
+}
+
 }
