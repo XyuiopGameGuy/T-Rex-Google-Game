40293a82ece5e3fe971a10e3495cee2945b3e4e1
XyuiopGameGuy <92894184+XyuiopGameGuy@users.noreply.github.com>
2021-11-08 20:08:01 -0300

    Update sketch.js

--- Trex_Stage_0-main/sketch.js
+++ Trex_Stage_0-main/sketch.js
@@ -1,13 +1,27 @@
   
+ 
  var trex;
  var trexAnimation;
  var ground;
  var chao;
  var chaoinvisivel;
+ var nuvem;
+ var playgame = 1;
+ var endgame = 0;
+ var gamestate;
+ var cloudgroup;
+ var obstaclegroup;
 
  function preload(){
   trexAnimation= loadAnimation("trex1.png", "trex3.png", "trex4.png");
   ground= loadImage("ground2.png");
+  nuvem= loadImage("cloud.png");
+  obstaculo1= loadImage("obstacle1.png");
+  obstaculo2= loadImage("obstacle2.png");
+  obstaculo3= loadImage("obstacle3.png");
+  obstaculo4= loadImage("obstacle4.png");
+  obstaculo5= loadImage("obstacle5.png");
+  obstaculo6= loadImage("obstacle6.png");
 }
 
 function setup(){
@@ -20,21 +34,78 @@ function setup(){
   chao.addImage(ground); 
   chaoinvisivel= createSprite(300, 190, 600, 10);
   chaoinvisivel.visible=false;
+
+    var rand =  Math.round(random(1,100))
+  console.log(rand)
+
+  cloudgroup= createGroup();
+  obstaclegroup= createGroup();
 }
 
 function draw(){
   background("white");
-  chao.velocityX= -5
-  if(keyDown("space") && trex.y > 152){
-    trex.velocityY=-13
-  }
-  trex.velocityY=trex.velocityY +0.8
-  if (chao.x < 0){
-    chao.x= chao.width/2;
-  } 
   trex.collide(chaoinvisivel);
-  console.log(trex.y);
-  drawSprites();
-}
+  console.log(frameCount);
 
+  if (gamestate == playgame){
+    if (chao.x < 0){
+      chao.x= chao.width/2;
+    } 
+    chao.velocityX= -10
+    if(keyDown("space") && trex.y > 152){
+      trex.velocityY=-13
+    }
+    trex.velocityY=trex.velocityY +0.8
+    console.log(trex.y);
+
+    if (trex.isTouching(obstaclegroup)) {
+      gamestate = endgame;
+    }
+
+    if (gamestate == endgame) {
+      chao.velocityX= 0;
+      trex.velocityY= 0;
+      cloud.velocityX= 0;
+      obstaculo.velocityX= 0;
+    }
+    spawnClouds();
+    createObstacles();
+    drawSprites();
+}
+function spawnClouds(){
+  if (frameCount %60 == 0){
+    cloud=createSprite(600,100,40,10);
+    cloudgroup.add(cloud);
+  cloud.addImage(nuvem);
+  cloud.y= Math.round(random(10, 60));
+  cloud.scale= 0.8;
+  cloud.velocityX=-3;
+  cloud.lifetime= 300;
+  cloud.depth = trex.depth;
+}
+ }
+function createObstacles(){
+  if (frameCount %80 == 0) {
+  obstaculo= createSprite(600, 160, 50, 50);
+  obstaculo.velocityX= -10;
+  var randObstacle= Math.round(random(1, 6));
+  obstaculo.scale = 1;
+  obstaculo.lifetime= 300;
+  obstaclegroup.add(obstaculo);
+ }
+  switch(randObstacle){
+   case 1: obstaculo.addImage(obstaculo1);
+   break;
+   case 2: obstaculo.addImage(obstaculo2);
+   break;
+   case 3: obstaculo.addImage(obstaculo3);
+   break;
+   case 4: obstaculo.addImage(obstaculo4);
+   break;
+   case 5: obstaculo.addImage(obstaculo5);
+   break;
+   case 6: obstaculo.addImage(obstaculo6);
+   default: break;
+  }
+}
 }
